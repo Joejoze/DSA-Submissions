d15120729ffc50b626e0cf32bdca4e138cac2a99
Joejoze <140178541+Joejoze@users.noreply.github.com>
2023-09-24 22:14:02 +0530

    Duplicate elements

---
+++ 25_09_Joel.c
@@ -0,0 +1,35 @@
+/* 
+Name :- Joel Jose
+Roll no :- 25
+Batch :- B    
+Program ID :- 09
+Description :- Program to count the total number of duplicate elements in an array. 
+*/
+
+#include <stdio.h>
+int main() 
+{
+    int size, i, j, count = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+        printf("Enter array elements:\n");
+    
+    for (i = 0; i < size; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < size; i++) 
+    {
+        for (j = i + 1; j < size; j++)
+        {
+                if (arr[i] == arr[j]) 
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    printf("Total duplicate elements: %d\n", count);
+}
